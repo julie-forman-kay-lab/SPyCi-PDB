c36ab6708fb4b5dfd0d156d2b7774623e6b81882
menoliu <nemoliu123@gmail.com>
2022-07-29 16:06:14 -0400

    logger and exceptions

--- src/spycipdb/__init__.py
+++ src/spycipdb/__init__.py
@@ -8,6 +8,7 @@ Logic imported from IDPConformerGenerator:
 https://github.com/julie-forman-kay-lab/IDPConformerGenerator/blob/3aef6b085ec09eeebc5812639a5eb6832c0215cd/src/idpconfgen/__init__.py
 """
 import logging
+import string
 from os import fspath, get_terminal_size
 from pathlib import Path as _Path
 
@@ -66,6 +67,19 @@ def assert_subclass(objs, subclass):
         if issubclass(subclass, type(obj)):
             return True
     return False
+
+
+def count_string_formatters(s):
+    """
+    Count string formatters: ``{}``.
+
+    Returns
+    -------
+    int
+        The number of string formatters.
+    """
+    assert isinstance(s, str), f'`s` of wrong type: {type(s)}'
+    return sum(1 for f in list(string.Formatter().parse(s)) if f[1] is not None)
 
 
 source_folder = Path(__file__).absparent
