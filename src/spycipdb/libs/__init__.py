6aadb400ea2a4983f02fe9a9f11c1f78031da448
menoliu <nemoliu123@gmail.com>
2022-09-13 12:39:39 -0400

    initialize data and tests

--- src/spycipdb/libs/__init__.py
+++ src/spycipdb/libs/__init__.py
@@ -1,1 +1,35 @@
 """General Libraries for the project."""
+import os
+from pathlib import Path as _Path
+
+class Path(type(_Path())):
+    """
+    Derived from:
+    https://github.com/joaomcteixeira/taurenmd/blob/4e087f0bdb7ea7b7b5ae70c589c73a72437f3de6/src/taurenmd/core.py
+    
+    Extends Python's `Path object`_ interface.
+    .. _Path object: https://docs.python.org/3/library/pathlib.html
+    """
+
+    def str(self):
+        """
+        Represent path as string.
+        Alias to ``os.fspath(self)``.
+        
+        Returns
+        -------
+        str
+           ``os.fspath(self)``.
+        """
+        return os.fspath(self)
+    
+    def myparents(self):
+        """
+        List of the path parent folders.
+        Alias to ``pathlib.Path.resolve().parents[0]``.
+        Returns
+        -------
+        list
+            Parent paths. Name file or folder are excluded.
+        """
+        return self.resolve().parents[0]
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,16 @@
-"""Test suite for spycipdb."""
+"""Contains general variables for spycipdb unit tests."""
+
+from spycipdb.libs import Path
+
+datap = Path(
+    Path(__file__).myparents(),
+    'data',
+    )
+
+jc_exp_expected = Path(datap, 'drksh3_JC.txt')
+noe_exp_expected = Path(datap, 'drksh3_NOE.txt')
+pre_exp_expected = Path(datap, 'drksh3_PRE.txt')
+fret_exp_expected = Path(datap, 'asyn_FRET.txt')
+
+drk_test = Path(datap, 'drksh3_conf.pdb')
+asyn_test = Path(datap, 'asyn_conf.pdb')
